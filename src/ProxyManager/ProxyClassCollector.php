d4c2e74e765ad5ae4e5f0f4170312e68c76ffa96
titrxw <1120309488@qq.com>
2021-09-06 16:36:05 +0800

    style: 修改变量命名

--- src/ProxyManager/ProxyClassCollector.php
+++ src/ProxyManager/ProxyClassCollector.php
@@ -1,13 +1,23 @@
 <?php
 
+/**
+ * Rangine Aspect
+ *
+ * (c) We7Team 2019 <https://www.w7.cc>
+ *
+ * This is not a free software
+ * Using it under the license terms
+ * visited https://www.w7.cc for more details
+ */
+
 namespace W7\Aspect\ProxyManager;
 
 class ProxyClassCollector {
 	protected static $classMap = [];
 
-	public static function addClassProxy($originClassName, $proxyClassPath) {
+	public static function addClassProxy($originClassName, $proxyClassName) {
 		$originClassName = ltrim($originClassName, '\\');
-		self::$classMap[$originClassName] = $proxyClassPath;
+		self::$classMap[$originClassName] = $proxyClassName;
 	}
 
 	public static function getProxyClass($originClassName) {
